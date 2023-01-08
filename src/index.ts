878194866d50315119c1d10ac80e32ac5e49e734
newwares <newwares123@gmail.com>
2023-01-07 22:29:38 -0500

    make disabled work

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import madge from "madge";
 if (((await (madge("./"))).circular()).length > 0) {
     console.error("Circular Dependencies found, please double check the madge graph");
 }
-console.log("Starting... (if it freezes here then there might be a circular dependency caused by dynamic imports");
 await madge("./")
     .then((res) => res.image("./dependencies.svg"));
+console.log("Starting... (if it freezes here then there might be a circular dependency caused by dynamic imports");
+
 import("./bot.js");
