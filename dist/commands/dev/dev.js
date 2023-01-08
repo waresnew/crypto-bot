6d0d06c494385916808bee513dbb75d37a5a340b
newwares <newwares123@gmail.com>
2023-01-08 10:02:13 -0500

    remove dist from git

--- dist/commands/dev/dev.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-export default {
-    data: new SlashCommandBuilder().setName("dev").setDescription("Run test commands"),
-    async execute (interaction) {
-        await interaction.reply("crazy");
-    }
-};
-
-//# sourceMappingURL=dev.js.map
