dabe783dc44a37b05e9090693e670311a461c8dc
newwares <newwares123@gmail.com>
2023-03-26 20:22:02 -0400

    rename uri to url

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const db = mongoose.connection;
 
 export async function openDb() {
-    await mongoose.connect(process.env["MONGO_URI"], {
+    await mongoose.connect(process.env["MONGO_URL"], {
         dbName: `crypto-bot-${process.env["NODE_ENV"]}`
     });
     db.once("open", () => {
