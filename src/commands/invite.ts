1c1f0972854e5d4db1f9493bdb47aabdb012ab05
newwares <newwares123@gmail.com>
2023-04-18 16:45:50 -0400

    add invite cmd

---
+++ src/commands/invite.ts
@@ -0,0 +1,29 @@
+import {
+    APIApplicationCommand,
+    APIInteractionResponse,
+    ApplicationCommandType,
+    InteractionResponseType
+} from "discord-api-types/v10";
+import {
+    APIChatInputApplicationCommandInteraction
+} from "discord-api-types/payloads/v10/_interactions/_applicationCommands/chatInput";
+import {FastifyReply} from "fastify";
+import {getEmbedTemplate} from "../ui/templates";
+
+export default {
+    name: "invite",
+    type: ApplicationCommandType.ChatInput,
+    description: "Add the bot to your server!",
+    async execute(interaction: APIChatInputApplicationCommandInteraction, http: FastifyReply) {
+        await http.send({
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+                embeds: [{
+                    ...getEmbedTemplate(),
+                    title: "Invite Botchain",
+                    description: "[Click to add me to your server!](https://discord.com/oauth2/authorize?client_id=1058388231273590885&scope=bot+applications.commands)"
+                }]
+            }
+        } as APIInteractionResponse);
+    }
+} as APIApplicationCommand;
